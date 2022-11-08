ab313456212fa28559fc29ceca59cc49484fba0a
Yoann Z <zhangyou09@outlook.com>
2022-11-08 20:45:14 +0800

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Yoann Zhang on 2022/11/8.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
